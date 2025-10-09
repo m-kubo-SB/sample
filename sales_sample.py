406e4edd132f9e758ae22d8ae2de4a12a7144fb8
m-kubo-SB <m-kubo@spaceblast.co.jp>
2025-10-09 19:07:38 +0900

    Update and rename e-ticket_asoview.py to sales_sample.py

--- sales_sample.py
+++ sales_sample.py
@@ -2,6 +2,7 @@ import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
+import japanize_matplotlib
 
 st.title("販売チャネル別　営業利益シュミレーションサンプル！")
 #日本語フォント設定
@@ -94,5 +95,6 @@ st.write({
     "イーチケ固定費" : F_eticket,
     "外部実質手数料率" : r_A_eff,
     "イーチケ手数料率" : r_eticket
+
 
 })
